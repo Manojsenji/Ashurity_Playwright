7d58f735785aaafc3eee65ceccebdb674596e2b4
manoj <manojsreenivas95@gmail.com>
2026-03-06 12:47:13 +0530

    Fix Allure reporter output folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,33 +1,31 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  globalSetup: require.resolve('./global-setup'),
-  
-  testDir: './tests',
+  globalSetup: require.resolve("./global-setup"),
+
+  testDir: "./tests",
   fullyParallel: true,
   retries: 0,
   workers: 3,
   reporter: [
-    ['html', {open: 'never'}],
-    ['junit', { outputFile: 'test-results/results.xml' }],
-    ['list'],
-    ['allure-playwright']
+    ["html", { open: "never" }],
+    ["junit", { outputFile: "test-results/results.xml" }],
+    ["list"],
+    ["allure-playwright", { outputFolder: "allure-results" }],
   ],
   use: {
     baseURL: process.env.BASE_URL,
     headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure',
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
     launchOptions: {
       slowMo: 200, // simulate human typing
       args: [
-        '--disable-autofill-keyboard-accessory-view',
-        '--disable-password-manager-reauthentication'
+        "--disable-autofill-keyboard-accessory-view",
+        "--disable-password-manager-reauthentication",
       ],
     },
   },
-  projects: [
-    { name: 'chromium', use: { browserName: 'chromium' } },
-  ],
+  projects: [{ name: "chromium", use: { browserName: "chromium" } }],
 });
